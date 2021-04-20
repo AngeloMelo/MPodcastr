a245d47a0c6ed2e38fea6ff5554e6e105b0d5b86
AngeloMelo <angelo.s.melo@gmail.com>
2021-04-20 18:25:51 -0300

    add player component

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,44 @@
+import styles from './styles.module.scss'
+
+const Player = () => {
+    
+    return (
+        <div className={styles.playerContainer}>
+            <header>
+                <img src="/playing.svg" alt="playing now"/>
+                <strong>Playing now</strong>
+            </header>
+            <div className={styles.emptyPlayer}>
+                <strong>Select a podcast to listen</strong>
+            </div>
+            <footer>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider}></div>
+                    </div>
+                    <span>00:00</span>
+                </div>
+                <div className={styles.buttons}>
+                    <button type="button">
+                        <img src="/shuffle.svg" alt="Shuffle"/>
+                    </button>
+                    <button type="button">
+                        <img src="/play-previous.svg" alt="Play Previous"/>
+                    </button>
+                    <button type="button" className={styles.playButton}>
+                        <img src="/play.svg" alt="Play"/>
+                    </button>
+                    <button type="button">
+                        <img src="/play-next.svg" alt="Play Next"/>
+                    </button>
+                    <button type="button">
+                        <img src="/repeat.svg" alt="Repeat"/>
+                    </button>
+                </div>
+            </footer>
+        </div>
+    )
+}
+
+export default Player
